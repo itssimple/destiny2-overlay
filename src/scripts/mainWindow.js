733a5ee08c50aa32d183e18e032d196ae32560dc
NoLifeKing <webmaster@itssimple.se>
2021-10-17 00:13:23 +0200

    feat: Added crude version of showing available goals for tracking. Shows triumps as of now

--- src/scripts/mainWindow.js
+++ src/scripts/mainWindow.js
@@ -10,6 +10,8 @@ const backgroundWindow = overwolf.windows.getMainWindow();
 /** @type EventEmitter */
 const eventEmitter = backgroundWindow.eventEmitter;
 
+const destinyBaseUrl = "https://www.bungie.net";
+
 const db = backgroundWindow.db;
 
 var overwolfAdvertiseObject = null;
@@ -45,8 +47,217 @@ function setLastPlayedCharacter(lastPlayed) {
 }
 
 eventEmitter.addEventListener("destiny-data-loaded", async function () {
-  await destinyApiClient.getNamedDataObject(true);
+  let namedObject = await destinyApiClient.getNamedDataObject(true);
+
+  await loadGoals("destiny-data-loaded", namedObject);
 });
+
+async function loadGoals(loadReason, namedObject) {
+  if (
+    !destinyApiClient.destinyDataDefinition.DestinyPresentationNodeDefinition
+  ) {
+    return;
+  }
+
+  let depth = 0;
+
+  let triumphPresentation =
+    destinyApiClient.destinyDataDefinition.DestinyPresentationNodeDefinition[
+      "1163735237"
+    ];
+
+  let goalContainer = document.querySelector("#allGoals");
+  goalContainer.innerHTML = "";
+
+  for (let childNode of triumphPresentation.children.presentationNodes) {
+    let node =
+      destinyApiClient.destinyDataDefinition.DestinyPresentationNodeDefinition[
+        childNode.presentationNodeHash
+      ];
+
+    let categoryHeader = document.createElement("div");
+    categoryHeader.classList.add("hud");
+    categoryHeader.classList.add("headers");
+    categoryHeader.classList.add("translucent");
+    categoryHeader.innerText = node.displayProperties.name;
+
+    goalContainer.appendChild(categoryHeader);
+
+    depth++;
+    renderSubPresentationNode(node, namedObject, depth);
+    depth--;
+  }
+}
+
+async function renderSubPresentationNode(presentationNode, namedObject, depth) {
+  let goalContainer = document.querySelector("#allGoals");
+
+  if (
+    presentationNode.children &&
+    presentationNode.children.presentationNodes.length > 0
+  ) {
+    for (let childNode of presentationNode.children.presentationNodes) {
+      let subNode =
+        destinyApiClient.destinyDataDefinition
+          .DestinyPresentationNodeDefinition[childNode.presentationNodeHash];
+
+      let goal = document.createElement("div");
+      goal.classList.add("hud");
+      goal.classList.add("sub-header");
+      goal.classList.add("translucent");
+      goal.innerHTML =
+        "&gt;&nbsp;".repeat(depth) + subNode.displayProperties.name;
+
+      goalContainer.appendChild(goal);
+
+      renderRecordNodes(subNode, namedObject);
+      depth++;
+      renderSubPresentationNode(subNode, namedObject, depth);
+      depth--;
+    }
+  }
+}
+
+async function renderRecordNodes(presentationNode, namedObject) {
+  if (
+    !presentationNode.children ||
+    presentationNode.children.records.length == 0
+  ) {
+    return;
+  }
+
+  let goalContainer = document.querySelector("#allGoals");
+
+  let recordContainer = document.createElement("div");
+  recordContainer.classList.add("goalkeeper");
+
+  for (let childNode of presentationNode.children.records) {
+    let subNode =
+      destinyApiClient.destinyDataDefinition.DestinyRecordDefinition[
+        childNode.recordHash
+      ];
+
+    let characterRecord =
+      namedObject.characterRecords.records[childNode.recordHash];
+
+    let recordInfo = destinyApiClient.mapHashesToDefinitionsInObject(subNode);
+
+    if (recordInfo) {
+      let objectivesHtml = "";
+
+      if (recordInfo.objectiveHashes) {
+        for (let objectiveHash of recordInfo.objectiveHashes) {
+          let objective =
+            destinyApiClient.destinyDataDefinition.DestinyObjectiveDefinition[
+              objectiveHash
+            ];
+
+          let objectiveInfo =
+            destinyApiClient.mapHashesToDefinitionsInObject(objective);
+
+          if (characterRecord && characterRecord.objectives) {
+            let objectiveRecord = characterRecord.objectives.find(
+              (o) => o.objectiveHash == objectiveHash
+            );
+
+            if (objectiveRecord) {
+              let objectiveHtml = `<div class="goal-objective">
+  <div class="checkbox block">
+    <label>
+      <input type="checkbox" disabled ${
+        objectiveRecord.complete ? "checked" : ""
+      } />
+      <span>${
+        objectiveInfo.progressDescription
+          ? objectiveInfo.progressDescription
+          : objectiveRecord.objectiveProgressDescription
+          ? objectiveRecord.objectiveProgressDescription
+          : "Completed"
+      }</span>
+  </div>
+</div>`;
+
+              if (
+                !objectiveInfo.progressDescription &&
+                !objectiveRecord.objectiveProgressDescription
+              ) {
+                console.log(
+                  recordInfo.displayProperties.name,
+                  objectiveInfo,
+                  objectiveRecord
+                );
+              }
+
+              objectivesHtml += objectiveHtml;
+            } else {
+              let objectiveHtml = `<div class="goal-objective">
+  <div class="checkbox block">
+    <label>
+      <input type="checkbox" disabled />
+      <span>${
+        objectiveInfo.progressDescription
+          ? objectiveInfo.progressDescription
+          : objectiveRecord.objectiveProgressDescription
+          ? objectiveRecord.objectiveProgressDescription
+          : "Completed"
+      }</span>
+  </div>
+</div>`;
+              if (
+                !objectiveInfo.progressDescription &&
+                !objectiveRecord.objectiveProgressDescription
+              ) {
+                console.log(
+                  recordInfo.displayProperties.name,
+                  objectiveInfo,
+                  objectiveRecord
+                );
+              }
+              objectivesHtml += objectiveHtml;
+            }
+          } else {
+            let objectiveHtml = `<div class="goal-objective">
+  <div class="checkbox block">
+    <label>
+      <input type="checkbox" disabled />
+      <span>${
+        objectiveInfo.progressDescription
+          ? objectiveInfo.progressDescription
+          : "Completed"
+      }</span>
+  </div>
+</div>`;
+            if (!objectiveInfo.progressDescription) {
+              console.log(
+                recordInfo.displayProperties.name,
+                objectiveInfo,
+                objective
+              );
+            }
+            objectivesHtml += objectiveHtml;
+          }
+        }
+      }
+
+      let record = document.createElement("div");
+      record.classList.add("goal");
+
+      record.innerHTML = `
+      <div class="goal-image">
+        <img src="${destinyBaseUrl}${recordInfo.displayProperties.icon}" />
+      </div>
+      <div class="goal-info">
+        <div class="goal-name">${recordInfo.displayProperties.name}</div>
+        <div class="goal-description">${recordInfo.displayProperties.description}</div>
+        ${objectivesHtml}
+      </div>`;
+
+      recordContainer.appendChild(record);
+    }
+  }
+
+  goalContainer.appendChild(recordContainer);
+}
 
 eventEmitter.addEventListener("destiny-not-authed", function () {
   document.querySelector("#authenticateWithBungie").style.display = "";
@@ -265,6 +476,8 @@ function bindExitButtonEvent(window) {
         document.querySelector("#authenticateWithBungie").style.display = "";
         document.querySelector("#logoutFromBungie").style.display = "none";
       }
+
+      await loadGoals("open-window");
     }, 100);
   });
 
