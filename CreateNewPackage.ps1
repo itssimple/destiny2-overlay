cdcd5c592bed106ef2be4d03f5a478b300faa2e3
NoLifeKing <webmaster@itssimple.se>
2022-04-14 13:42:48 +0200

    fix: Accidentally removed disable_auto_dpi_sizing

--- CreateNewPackage.ps1
+++ CreateNewPackage.ps1
@@ -1,5 +1,4 @@
 Remove-Item .\destiny2-tracker.opk, .\destiny2-tracker.zip -ErrorAction Ignore
 Write-Output "Compressing new version into archive, please hold on"
-#Compress-Archive -Path .\manifest.json,.\resources,.\windows -DestinationPath .\game-time-tracker.zip
 7z a -tzip .\destiny2-tracker.opk -r @package-source.lst -xr!node_modules -xr!src
 Write-Output "New OPK generated"
